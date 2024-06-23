f76a26e235a39f41701360b7f72da9bb6eecdb5b
fiseyy <adrianwalker3004@gmail.com>
2024-06-23 17:41:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 # Функция для сохранения и загрузки данных о балансе пользователей
