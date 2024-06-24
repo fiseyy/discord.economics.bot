49b6c3205dc9a62a3c566ae1717fcfb14184b877
fiseyy <adrianwalker3004@gmail.com>
2024-06-24 11:50:01 +0300

    Big update
    добавил возможность работать раз в 20 минут, добавил пару предметов

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import json
 import os
+import random
+from discord.ext.commands import cooldown
 
 intents = discord.Intents.default()
 intents.members = True
@@ -55,6 +57,74 @@ async def add_coins(ctx, member: discord.Member, amount: int):
     await ctx.send(f"{member.mention}, вам было добавлено {amount} монет.")
 
 
+@bot.command()
+async def work_list(ctx):
+    embed = discord.Embed(
+        title="Список работ",
+        description="Выберите работу",
+    )
+    embed.add_field(name="банкир", value="100 монет", inline=True)
+    embed.add_field(name="полицейский", value="500 монет шансом 66%", inline=True)
+    embed.add_field(name="пожарный", value="300 монет шансом 87%", inline=True)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+@cooldown(1, 1200, commands.BucketType.user)
+async def work(ctx, work_type: str):
+    data = load_data()
+    user_id = str(ctx.author.id)
+    if user_id not in data:
+        data[user_id] = 0
+        save_data(data)
+    balance = data.get(user_id, 0)
+    match work_type:
+        case "банкир":
+            data[user_id] += 100
+            f"{ctx.author.mention}, Вы заработали 100 монет."
+        case "полицейский":
+            # шанс 33% потерять 75% денег
+            data[user_id] += 500
+            if random.randint(1, 3) == 1:
+                data[user_id] -= int(500 * 0.75)
+                await ctx.send(
+                    f"{ctx.author.mention}, О нет! Преступник украл у вас часть вашей зарплаты! Вы заработали 125 монет."
+                )
+            else:
+                await ctx.send(
+                    f"{ctx.author.mention}, Вы молодец! Вы заработали 500 монет."
+                )
+        case "пожарный":
+            # шанс 12% потерять 60% денег
+            data[user_id] += 300
+            if random.randint(1, 8) == 1:
+                data[user_id] -= int(300 * 0.60)
+                await ctx.send(
+                    f"{ctx.author.mention}, О нет! У вас при пожаре выпал кошелек! Вы заработали 120 монет."
+                )
+            else:
+                await ctx.send(
+                    f"{ctx.author.mention}, Вы молодец! Вы заработали 300 монет."
+                )
+        case _:
+            await ctx.send(
+                f"{ctx.author.mention}, неизвестная работа. Пожалуйста, выберите из списка работ. (!work_list)"
+            )
+    save_data(data)
+
+
+@work.error
+async def work_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        cooldown_time = error.retry_after
+        minutes, seconds = divmod(cooldown_time, 60)
+        await ctx.send(
+            f"Вы должны подождать {round(minutes,0)} минут и {round(seconds, 0)} секунд перед использованием этой команды снова."
+        )
+    else:
+        raise error
+
+
 # Команда для покупки предмета
 @bot.command()
 async def buy(ctx, item_name: str):
@@ -67,8 +137,9 @@ async def buy(ctx, item_name: str):
 
     # Список предметов и их стоимость
     store_items = {
-        "item1": 100,
-        "item2": 200,
+        "телефон": 100,
+        "планшет": 200,
+        "ноутбук": 500,
         # Добавьте больше предметов и их стоимость здесь
     }
 
